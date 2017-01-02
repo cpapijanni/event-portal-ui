47c172b3cc364eb3cb2d6d3053e63996fe8caba8
cpapijanni <pcnr007@gmail.com>
2017-01-02 23:30:55 +0530

    Changes for Router Navigaion in Header and Footer

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'layout-header',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {}
+  
+  navigateTo(path: string){
+	this.router.navigate([path]); 
+  }
 }
